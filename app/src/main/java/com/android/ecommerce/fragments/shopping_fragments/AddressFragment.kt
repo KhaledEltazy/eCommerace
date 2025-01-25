53c1c3a7d1e71c65ce764b70c4e64a4751238063
khaled <khmeltazy@gmail.com>
2025-01-25 14:26:52 +0200

    implementing address fragment and its viewModel

---
+++ app/src/main/java/com/android/ecommerce/fragments/shopping_fragments/AddressFragment.kt
@@ -0,0 +1,72 @@
+package com.android.ecommerce.fragments.shopping_fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.android.ecommerce.R
+import com.android.ecommerce.data.Address
+import com.android.ecommerce.databinding.FragmentAddressBinding
+import com.android.ecommerce.util.Resource
+import com.android.ecommerce.viewmodel.AddressViewModel
+import kotlinx.coroutines.launch
+
+
+class AddressFragment : Fragment() {
+    private lateinit var binding : FragmentAddressBinding
+    val viewModel by viewModels<AddressViewModel>()
+        override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+            binding = FragmentAddressBinding.inflate(inflater)
+            return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            saveBtnddressFragment.setOnClickListener {
+                val addressTitle = addressETAddressFragment.text.toString()
+                val fullName = fullNameETAddressFragment.text.toString()
+                val phone = phoneETAddressFragment.text.toString()
+                val street = streetETAddressFragment.text.toString()
+                val state = stateETAddressFragment.text.toString()
+                val city = cityETAddressFragment.text.toString()
+
+                val address = Address(addressTitle,fullName,phone,street,state,city)
+
+                viewModel.addAddress(address)
+            }
+        }
+
+        lifecycleScope.launch {
+            viewModel.addAddress.collect{
+                when(it){
+                    is Resource.Loading ->{
+                        binding.saveBtnddressFragment.startAnimation()
+                    }
+                    is Resource.Success ->{
+                        binding.saveBtnddressFragment.revertAnimation()
+                    }
+                    is Resource.Error ->{
+                        binding.saveBtnddressFragment.revertAnimation()
+                        Toast.makeText(requireContext(),it.message.toString(),Toast.LENGTH_LONG).show()
+                    }
+                    else -> Unit
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            viewModel.error.collect{
+                Toast.makeText(requireContext(),it,Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/android/ecommerce/viewmodel/AdressViewModel.kt
@@ -0,0 +1,62 @@
+package com.android.ecommerce.viewmodel
+
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android.ecommerce.data.Address
+import com.android.ecommerce.util.Resource
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AddressViewModel @Inject constructor(
+    private val firestore: FirebaseFirestore,
+    private val auth: FirebaseAuth
+) : ViewModel() {
+
+    private val _addAddress = MutableStateFlow<Resource<Address>>(Resource.Unspecified())
+    val addAddress = _addAddress.asStateFlow()
+
+    private val _error = MutableSharedFlow<String>()
+    val error = _error.asSharedFlow()
+
+    fun addAddress(address : Address){
+        val validateInput = validateInput(address)
+        if (validateInput) {
+            viewModelScope.launch { _addAddress.emit(Resource.Loading()) }
+            firestore.collection("user").document(auth.uid!!).collection("address").document()
+                .set(address)
+                .addOnSuccessListener {
+                    viewModelScope.launch {
+                        _addAddress.emit(Resource.Success(address))
+                    }
+                }
+                .addOnFailureListener {
+                    viewModelScope.launch {
+                        _addAddress.emit(Resource.Error(it.message.toString()))
+                    }
+                }
+        } else {
+            viewModelScope.launch {
+                _error.emit("All Fields are required")
+            }
+        }
+    }
+
+    private fun validateInput(address: Address) : Boolean {
+        return address.addressTitle.trim().isNotEmpty() &&
+                address.fullName.trim().isNotEmpty() &&
+                address.street.trim().isNotEmpty() &&
+                address.phone.trim().isNotEmpty() &&
+                address.city.trim().isNotEmpty() &&
+                address.state.trim().isNotEmpty()
+
+    }
+}
